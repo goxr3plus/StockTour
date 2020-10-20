4ea3e2395a836643e992019aa6d4dd03f520554c
goxr3plus <crAlexcomp@gmail.com>
2020-10-20 22:00:36 +0300

    ui: Fixed Footer Message

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import { Navbar, Nav, NavItem } from 'reactstrap';
-
-import SourceLink from 'components/SourceLink';
+import { Nav, Navbar, NavItem } from 'reactstrap';
 
 const Footer = () => {
   return (
     <Navbar>
       <Nav navbar>
         <NavItem>
-          2018 Reduction theme, source on <SourceLink>Github</SourceLink>
+          {/*2018 Reduction theme, source on <SourceLink>Github</SourceLink>*/}
+          &copy; Copyright {new Date().getFullYear()} <a href={''} target="_blank">GOXR3PLUS STUDIO</a>
         </NavItem>
       </Nav>
     </Navbar>
